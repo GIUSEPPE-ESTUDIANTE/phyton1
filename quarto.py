77547c296fad13285a649199d21592bcce3098b7
giuseppe costa <giusepe002@gmail.com>
2024-09-25 08:28:07 -0300

    primeiros codigos em pyhton

---
+++ quarto.py
@@ -0,0 +1,10 @@
+numero1 = input("Digite um valor:")
+numero2 = input("DIgite outro valor:")
+# usamos o comando int para converter as variaveis
+# numero1 e numero em valores inteiros, pois
+# o retorno do comando input é sempre uma 
+# string
+
+resultado = int(numero1) + int(numero2)
+
+print(resultado)
